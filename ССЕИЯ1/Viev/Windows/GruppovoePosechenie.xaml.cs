489ee7bb556679c2e1ad8a93cc3a558d329b2e59
yavshkole2018@mail.ru <yavshkole2018@mail.ru>
2023-03-15 11:07:55 +0300

    11

--- ССЕИЯ1/Viev/Windows/GruppovoePosechenie.xaml.cs
+++ ССЕИЯ1/Viev/Windows/GruppovoePosechenie.xaml.cs
@@ -23,8 +23,74 @@ namespace ССЕИЯ1.Viev.Windows
         {
             InitializeComponent();
             PodrazdCmb.SelectedItem = "ID";
-            PodrazdCmb.DisplayMemberPath = "Name";
-            PodrazdCmb.ItemsSource = Helpers.BD.user24Entities.Organization.ToList();
+            PodrazdCmb.DisplayMemberPath = "DivisionName";
+            PodrazdCmb.ItemsSource = Helpers.BD.user24Entities.Divisions.ToList();
+
+
+            txtOrganizaci.SelectedItem = "ID";
+            txtOrganizaci.DisplayMemberPath = "Name";
+            txtOrganizaci.ItemsSource = Helpers.BD.user24Entities.Organization.ToList();
+        }
+
+        private void PodrazdCmb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string log = "";
+            if (string.IsNullOrWhiteSpace(TxtFamiliya.Text))
+                log += "Введите фамилию\n";
+
+            if (string.IsNullOrWhiteSpace(TXTIMYA.Text))
+                log += "Введите имя\n";
+
+            if (string.IsNullOrWhiteSpace(TxtOtcestvo.Text))
+                log += "Повторите отчество\n";
+            if (string.IsNullOrWhiteSpace(txtPhone.Text))
+                log += "Введите телефон\n";
+            if (string.IsNullOrWhiteSpace(TxtPochta.Text))
+                log += "Введите почту\n";
+            if (string.IsNullOrWhiteSpace(txtOrganizaci.Text))
+                log += "Выберите организациию\n";
+            if (string.IsNullOrWhiteSpace(TxtSeria.Text))
+                log += "Введите серию\n";
+            if (string.IsNullOrWhiteSpace(TxtNomer.Text))
+                log += "Введите номер\n";
+
+
+            if (log != "")
+            {
+                MessageBox.Show(log);
+                log = "";
+                return;
+            }
+
+
+            Models.InfoClient profile = new Models.InfoClient()
+            {
+                Surname = TxtFamiliya.Text,
+                Name = TXTIMYA.Text,
+                MiddleName = TxtOtcestvo.Text,
+                Phone = txtPhone.Text,
+                Email = TxtPochta.Text,
+                Organization = txtOrganizaci.SelectedItem as Models.Organization,
+                Comment = TxtPrimechanie.Text,
+                SeriyaPasport = int.Parse(TxtSeria.Text),
+                numberPasport = int.Parse(TxtNomer.Text),
+                DataBirthday = (DateTime)DatePickerBerth.SelectedDate
+
+
+            };
+            Helpers.BD.user24Entities.InfoClient.Add(profile);
+            Helpers.BD.user24Entities.SaveChanges();
+            MessageBox.Show("Новый клиент успешно добавлен");
         }
     }
 }
--- ССЕИЯ1/Viev/Windows/LichoePosechenie.xaml.cs
+++ ССЕИЯ1/Viev/Windows/LichoePosechenie.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,15 +24,107 @@ namespace ССЕИЯ1.Viev.Windows
         public LichoePosechenie()
         {
             InitializeComponent();
+            CoMbo_podrazdel.SelectedItem = "ID";
+            CoMbo_podrazdel.DisplayMemberPath = "DivisionName";
+            CoMbo_podrazdel.ItemsSource = Helpers.BD.user24Entities.Divisions.ToList();
 
-            COMBO.SelectedValuePath = "ID";
-            CoMbo_podrazdel.DisplayMemberPath = "Name";
-            CoMbo_podrazdel.ItemsSource = Helpers.BD.user24Entities.Authorizations.ToList();
+
+            TxtOrgan.SelectedItem = "ID";
+            TxtOrgan.DisplayMemberPath = "Name";
+            TxtOrgan.ItemsSource = Helpers.BD.user24Entities.Organization.ToList();
+
+            COMBO.SelectedItem = "ID";
+            COMBO.DisplayMemberPath = "Visiting1";
+            COMBO.ItemsSource = Helpers.BD.user24Entities.Visiting.ToList();
         }
 
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }
+
+        private void CoMbo_podrazdel_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            TxtFami.Clear();
+            TxtName.Clear();
+            TxtOtchestvo.Clear();
+            TxtPhone.Clear();
+            TxtMail.Clear();
+            TxtSeria.Clear();
+            TxtNumber.Clear();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string log = "";
+            if (string.IsNullOrWhiteSpace(TxtFami.Text))
+                log += "Введите фамилию\n";
+
+            if (string.IsNullOrWhiteSpace(TxtName.Text))
+                log += "Введите имя\n";
+
+            if (string.IsNullOrWhiteSpace(TxtOtchestvo.Text))
+                log += "Повторите отчество\n";
+            if (string.IsNullOrWhiteSpace(TxtPhone.Text))
+                log += "Введите телефон\n";
+            if (string.IsNullOrWhiteSpace(TxtMail.Text))
+                log += "Введите почту\n";
+            if (string.IsNullOrWhiteSpace(TxtOrgan.Text))
+                log += "Выберите организациию\n";
+            if (string.IsNullOrWhiteSpace(TxtSeria.Text))
+                log += "Введите серию\n";
+            if (string.IsNullOrWhiteSpace(TxtNumber.Text))
+                log += "Введите номер\n";
+
+
+            if (log != "")
+            {
+                MessageBox.Show(log);
+                log = "";
+                return;
+            }
+
+
+            Models.InfoClient profile = new Models.InfoClient()
+            {
+                Surname = TxtFami.Text,
+                Name = TxtName.Text,
+                MiddleName = TxtOtchestvo.Text,
+                Phone = TxtPhone.Text,
+                Email = TxtMail.Text,
+                Organization = TxtOrgan.SelectedItem as Models.Organization,
+                Comment = TxtPrim.Text,
+                SeriyaPasport = int.Parse(TxtSeria.Text),
+                numberPasport = int.Parse(TxtNumber.Text),
+                DataBirthday = (DateTime)DateBirth.SelectedDate,
+                Photo = qwe
+
+            };
+            Helpers.BD.user24Entities.InfoClient.Add(profile);
+            Helpers.BD.user24Entities.SaveChanges();
+            MessageBox.Show("Новый клиент успешно добавлен");
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.ShowDialog();
+
+            var qw = openFileDialog.FileName;
+            qwe = File.ReadAllBytes(qw);
+        }
+
+        public byte[] qwe;
+
+        private void TxtOrgan_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
     }
 }
+
